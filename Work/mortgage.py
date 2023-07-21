e33f9683c4e7eb1dfcc82cfd5c6cd0313c359e3d
Jason Deng <jason.deng@tempus.com>
2023-07-21 16:18:27 -0500

    add extra pay

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,16 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month = 0
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+    if month < 12:
+        principal = principal * (1+rate/12) - (payment + 1000)
+        total_paid = total_paid + payment + 1000
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    month += 1
 
 print('Total paid', total_paid)
+print('Total months', month)
